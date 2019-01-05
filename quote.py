cbc1c329fcede66633c3aec3607986a38e5af773
Jayeshdahiwale <46401270+Jayeshdahiwale@users.noreply.github.com>
2019-01-05 21:12:26 +0530

    Add files via upload

---
+++ quote.py
@@ -0,0 +1,18 @@
+from locators.quote_locator import QuoteLocator
+class QuoteParser:
+    def __init__(self,parent):
+        self.parent= parent
+    def __repr__(self):
+        return f'Quote{self.content},by {self.author}'
+    @property
+    def content(self):
+        locator=QuoteLocator.CONTENT
+        return self.parent.select_one(locator).string
+    @property
+    def author(self):
+        locator=QuoteLocator.AUTHOR
+        return self.parent.select_one(locator).string
+    @property
+    def tags(self):
+        locator=QuoteLocator.TAGS
+        return[e.string for e in self.parent.select_one(locator)]
